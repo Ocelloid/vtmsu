8ea26c116be40d47a5fc909b40082003a1fe3dc5
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-04-24 02:48:01 +0500

    settings email fix

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -72,7 +72,7 @@ export default function Settings() {
     value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
 
   const isEmailInvalid = useMemo(() => {
-    if (userEmail === "") return false;
+    if (userEmail === "") return true;
     return validateEmail(userEmail) ? false : true;
   }, [userEmail]);
 
