896004403909f871c37f187ec51e0a41cd7aa8e8
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-07-23 16:58:39 +0500

    страница персонажа в разработке

--- src/pages/characters/[pid]/edit.tsx
+++ src/pages/characters/[pid]/edit.tsx
@@ -1,1 +1,9 @@
-export { default } from "~/components/editors/CharacterEditor";
+// export { default } from "~/components/editors/CharacterEditor";
+
+export default function NewCharacterPage() {
+  return (
+    <div className="flex h-[80vh] w-[100vw] items-center justify-center">
+      Страница редактирования персонажа находится в разработке до 5 августа
+    </div>
+  );
+}
