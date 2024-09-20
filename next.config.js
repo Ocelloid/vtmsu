f94eccf346df5b522c270f7b86a71b0328863128
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-09-20 22:16:58 +0500

    try mailru thingie

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx", "html"],
   reactStrictMode: true,
   images: {
     remotePatterns: [
