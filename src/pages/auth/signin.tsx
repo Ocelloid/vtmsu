d8f382d3d3db26581fef8a2cc37b4b3a04beda80
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-09-21 19:27:46 +0500

    перенаправление в настройки после верификации

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -5,6 +5,7 @@ import { useState, useMemo } from "react";
 import { signIn, useSession } from "next-auth/react";
 import { LoadingSpinner, LoadingPage } from "~/components/Loading";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function SignIn() {
   const router = useRouter();
@@ -29,97 +30,104 @@ export default function SignIn() {
   if (status === "loading") return <LoadingPage />;
 
   return (
-    <div className="flex h-full w-full items-center justify-center ">
-      <Card className="max-w-sm bg-red-950/50 text-red-100">
-        <CardHeader className="justify-center text-center text-xl font-semibold">
-          Вход в аккаунт
-        </CardHeader>
-        <CardBody className="justify-center gap-2 text-center">
-          Войдите, используя электронную почту или аккаунт в социальных сетях
-          <Input
-            variant="bordered"
-            className="min-w-28 max-w-96"
-            aria-label="Email"
-            placeholder="Введите электронную почту"
-            errorMessage="Некорректный адрес электронной почты"
-            isInvalid={emailError}
-            value={email}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !!email && !isEmailInvalid) {
-                setSigningIn(true);
-                void signIn("email", {
-                  email,
-                  callbackUrl: "/settings",
-                  redirect: true,
-                });
-              }
-            }}
-            onValueChange={setEmail}
-          />
-          <div
-            className="flex flex-col"
-            onClick={() => {
-              if (isEmailInvalid || !email) flashError();
-            }}
-          >
-            <Button
-              variant="solid"
-              isDisabled={!email || isEmailInvalid}
+    <>
+      <Head>
+        <title>Маскарад Вампиров</title>
+        <meta name="description" content="Маскарад Вампиров" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex h-full w-full items-center justify-center ">
+        <Card className="max-w-sm bg-red-950/50 text-red-100">
+          <CardHeader className="justify-center text-center text-xl font-semibold">
+            Вход в аккаунт
+          </CardHeader>
+          <CardBody className="justify-center gap-2 text-center">
+            Войдите, используя электронную почту или аккаунт в социальных сетях
+            <Input
+              variant="bordered"
+              className="min-w-28 max-w-96"
+              aria-label="Email"
+              placeholder="Введите электронную почту"
+              errorMessage="Некорректный адрес электронной почты"
+              isInvalid={emailError}
+              value={email}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && !!email && !isEmailInvalid) {
+                  setSigningIn(true);
+                  void signIn("email", {
+                    email,
+                    callbackUrl: "/settings",
+                    redirect: true,
+                  });
+                }
+              }}
+              onValueChange={setEmail}
+            />
+            <div
+              className="flex flex-col"
               onClick={() => {
-                setSigningIn(true);
-                void signIn("email", {
-                  email,
-                  callbackUrl: "/settings",
-                  redirect: true,
-                });
+                if (isEmailInvalid || !email) flashError();
               }}
             >
-              {signingIn ? <LoadingSpinner height={24} /> : "Войти"}
-            </Button>
-          </div>
-          <div className="flex justify-between gap-2">
-            <Button
-              title="Войти через Discord"
-              variant="ghost"
-              className="w-full"
-              onClick={() =>
-                signIn("discord", {
-                  callbackUrl: "/settings",
-                  redirect: true,
-                })
-              }
-            >
-              <FaDiscord size={24} />
-            </Button>
-            <Button
-              title="Войти через ВКонтакте"
-              variant="ghost"
-              className="w-full"
-              onClick={() =>
-                signIn("vk", {
-                  callbackUrl: "/settings",
-                  redirect: true,
-                })
-              }
-            >
-              <FaVk size={24} />
-            </Button>
-            <Button
-              title="Войти через Яндекс"
-              variant="ghost"
-              className="w-full"
-              onClick={() =>
-                signIn("yandex", {
-                  callbackUrl: "/settings",
-                  redirect: true,
-                })
-              }
-            >
-              <FaYandex size={18} />
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+              <Button
+                variant="solid"
+                isDisabled={!email || isEmailInvalid}
+                onClick={() => {
+                  setSigningIn(true);
+                  void signIn("email", {
+                    email,
+                    callbackUrl: "/settings",
+                    redirect: true,
+                  });
+                }}
+              >
+                {signingIn ? <LoadingSpinner height={24} /> : "Войти"}
+              </Button>
+            </div>
+            <div className="flex justify-between gap-2">
+              <Button
+                title="Войти через Discord"
+                variant="ghost"
+                className="w-full"
+                onClick={() =>
+                  signIn("discord", {
+                    callbackUrl: "/settings",
+                    redirect: true,
+                  })
+                }
+              >
+                <FaDiscord size={24} />
+              </Button>
+              <Button
+                title="Войти через ВКонтакте"
+                variant="ghost"
+                className="w-full"
+                onClick={() =>
+                  signIn("vk", {
+                    callbackUrl: "/settings",
+                    redirect: true,
+                  })
+                }
+              >
+                <FaVk size={24} />
+              </Button>
+              <Button
+                title="Войти через Яндекс"
+                variant="ghost"
+                className="w-full"
+                onClick={() =>
+                  signIn("yandex", {
+                    callbackUrl: "/settings",
+                    redirect: true,
+                  })
+                }
+              >
+                <FaYandex size={18} />
+              </Button>
+            </div>
+          </CardBody>
+        </Card>
+      </main>
+    </>
   );
 }
--- src/pages/auth/verify.tsx
+++ src/pages/auth/verify.tsx
@@ -1,16 +1,29 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Verify() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  if (session) void router.replace("/settings");
   return (
-    <div className="flex h-full w-full items-center justify-center ">
-      <Card className="max-w-sm bg-red-950/50 text-red-100">
-        <CardHeader className="justify-center text-center text-xl font-semibold">
-          Подтверждение почты
-        </CardHeader>
-        <CardBody className="justify-center text-center">
-          На вашу электронную почту выслано письмо с ссылкой для подтверждения
-        </CardBody>
-      </Card>
-    </div>
+    <>
+      <Head>
+        <title>Маскарад Вампиров</title>
+        <meta name="description" content="Маскарад Вампиров" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex h-full w-full items-center justify-center ">
+        <Card className="max-w-sm bg-red-950/50 text-red-100">
+          <CardHeader className="justify-center text-center text-xl font-semibold">
+            Подтверждение почты
+          </CardHeader>
+          <CardBody className="justify-center text-center">
+            На вашу электронную почту выслано письмо с ссылкой для подтверждения
+          </CardBody>
+        </Card>
+      </main>
+    </>
   );
 }
