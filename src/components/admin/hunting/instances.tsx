5c2683cb398b8e8a03a12be4f7afd1c0e507a974
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-10-03 23:55:31 +0500

    границы города при создании целей на карте

--- src/components/admin/hunting/instances.tsx
+++ src/components/admin/hunting/instances.tsx
@@ -15,7 +15,13 @@ import {
 } from "@nextui-org/react";
 import Image from "next/image";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 import L, { LatLng } from "leaflet";
 import { MapControl, Draggable } from "~/components/map";
 import type { HuntingInstance, HuntingData } from "~/server/api/routers/hunt";
@@ -27,6 +33,29 @@ import { api } from "~/utils/api";
 
 const marker_icon = L.icon({ iconUrl: "/map-marker.png" });
 const skull_icon = L.icon({ iconUrl: "/skull.png" });
+const cityBorders = [
+  new LatLng(57.984261983475534, 56.15550041198731),
+  new LatLng(57.99408935391382, 56.157903671264656),
+  new LatLng(57.99572698684907, 56.16657257080079),
+  new LatLng(58.00345924168589, 56.16906166076661),
+  new LatLng(58.00382307136166, 56.16253852844238),
+  new LatLng(58.00659715104656, 56.16331100463868),
+  new LatLng(58.01282667756441, 56.16356849670411),
+  new LatLng(58.02873660320649, 56.27188682556153),
+  new LatLng(58.02241893691665, 56.285276412963874),
+  new LatLng(58.01705486238637, 56.292743682861335),
+  new LatLng(58.0100985512297, 56.29411697387696),
+  new LatLng(58.00782495374456, 56.30012512207032),
+  new LatLng(58.004550719619765, 56.29755020141602),
+  new LatLng(57.99513562803938, 56.30484580993653),
+  new LatLng(57.9873560760959, 56.29231452941895),
+  new LatLng(57.985445050445875, 56.26956939697266),
+  new LatLng(57.98499002931178, 56.24373435974122),
+  new LatLng(57.98576356180083, 56.23643875122071),
+  new LatLng(57.984762516577405, 56.226911544799805),
+  new LatLng(57.97044906155012, 56.171636581420906),
+  new LatLng(57.984261983475534, 56.15550041198731),
+];
 
 const Instances = () => {
   const [targets, setTargets] = useState<HuntingData[]>([]);
@@ -260,6 +289,11 @@ const Instances = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <MapControl />
+        <Polyline
+          smoothFactor={3}
+          pathOptions={{ color: "red", dashArray: "10, 10", dashOffset: "10" }}
+          positions={cityBorders}
+        />
         <Draggable updatePosition={(p) => setPosition(p)} />
         {instances.map((instance) => (
           <Marker
